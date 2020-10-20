b7a5c416a85017b9712c6bcfd7c782b3634725c2
marandaz <mrzwies@gmail.com>
2020-10-19 19:19:36 -0500

    removed all notes/comments logged in script.js

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,15 +1,10 @@
-// 
-// ----- Handles Use Inputs-------
-// array to contain strings of ingredients provided by user
+
 let ingredientsArr = [];
-// this adds ingredients to the table
+
 $("#ingredientAdd").on("click", function () {
     let ingredient = $("#ingredientInput").val();
-    // checks if the input is not empty
     if (ingredient != "") {
-        // pushes ingredient into the array of ingredients
         ingredientsArr.push(ingredient);
-        // creates and appends several elements to make the ingredients list elements
         $("#ingredientsTable").append(
             $("<tr>").append(
                 $("<td>").text(ingredient)
@@ -19,25 +14,21 @@ $("#ingredientAdd").on("click", function () {
                 )
             )
         );
-        // clears the ingredients search bar
         $("#ingredientInput").val('');
-        // makes the ingredients table and search button appear
         $("#afterSearchContainer").removeClass("is-hidden")
     }
 });
-// will delete items from the user made ingredient list
 
 document.getElementById("ingredientsTable").addEventListener("click", function (event) {
     if (event.target.matches("a")) {
         event.target.parentElement.parentElement.remove();
     }
 
-    // this removes the ingredients table and search bar after all ingredients have been removed
     if (document.getElementById("ingredientsTable").rows.length == 1) {
         $("#afterSearchContainer").addClass("is-hidden")
     }
 });
-// -------------------------------
+
 function buildIngredientsURL() {
     let ingredientsAPIURL = "https://api.spoonacular.com/recipes/findByIngredients?"
     let ingredientsString = localStorage.getItem("ingredients")
@@ -47,7 +38,7 @@ function buildIngredientsURL() {
     };
     return ingredientsAPIURL + $.param(ingredientsParams);
 }
-// ---------- Handles Transfering User Input to Results ---
+
 $("#ingredientSearch").on("click", function () {
     localStorage.setItem("ingredients", ingredientsArr);
     let queryURL = "https://official-joke-api.appspot.com/random_joke";
